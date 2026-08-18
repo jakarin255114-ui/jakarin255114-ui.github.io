963e4dbc9a6d59737660f65a3e87a0515801f133
jakarin255114-ui <jakarin2551.14@gmail.com>
2026-08-18 11:13:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
 document.addEventlistener('DOMContentLoaded',function () {
+    document.getElementById('year').innerHTML = new Date().getFullYear();
     console.log('Portfolio ของ Jakarin พร้มอแล้ว!');
 });
